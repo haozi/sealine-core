0748db16c0c53417141abdeb32edbed2649c6e17
haozhime <a@div.li>
2016-09-28 17:27:21 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+/*!
+ * safe-trim v1.0.7
+ * (c) 2016 Jerry
+ * Released under the MIT License.
+ */
+'use strict';
+
+function safeTrim(string) {
+  alert(3 + 9);
+}
+
+module.exports = safeTrim;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default function safeTrim(string) {
+  alert(3 + 9)
+}
